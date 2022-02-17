406af52c49ebb0be88ffe2b5c4e0e55fdad9e659
EmmanoelCoutinho <emmanoelcesar967@gmail.com>
2022-02-17 01:09:15 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, MainContainer, Header, Main } from "./styled/app";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <MainContainer>
+        <Header>
+          <img className="swords" alt="swords_logo" src="/images/swords.svg" />
+          <a href="#">Home</a>
+          <a href="#">Skills</a>
+          <a href="#">Skins</a>
+        </Header>
+        <Main>
+          <div className="left_content ">
+            <img src="/images/yasuo_name.svg" alt="yasuo name" />
+            <h3></h3>
+            <p>
+              Yasuo, um ioniano extremamente determinado, é também um ágil
+              espadachim que usa o próprio ar como arma para enfrentar seus
+              inimigos. Quando jovem, ele teve seu orgulho ferido ao ser acusado
+              injustamente do assassinato de seu mestre.
+            </p>
+            <div className="buttons">
+              <button className="play_now">Jogue Agora</button>
+              <button className="login">Iniciar Sessão</button>
+            </div>
+          </div>
+          <div className="right_content">
+            <img
+              src="/images/nightbringer_yasuo.png"
+              alt="nightbringer yasuo"
+            />
+          </div>
+        </Main>
+      </MainContainer>
+    </Container>
   );
 }
 
